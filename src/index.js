fdbc531265cba14403a6b20647e38bdc6a174f78
Leollucena <leonardohlucena4@gmail.com>
2024-09-13 21:53:16 -0300

    troca de foto do cachorro

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-/*function scrollar(event){
-    event.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-const target =this.getAttribut('href').substring(1);
-const targetSection =document.getElementById(target);
-
-targetSection.scrollIntoView({ behavior: 'smooth' });
-}
-
-const LinkDeTrabalho = document.querySelector('a[href="#trabalho"]');
-const LinkDeLocation = document.querySelector('a[href="#location"]');
-const LinkDeFooter = document.querySelector('a[href="#footer"]');
-
-LinkDeTrabalho.addEventListener('click',scrollar);
-LinkDeLocation.addEventListener('click',scrollar);
-LinkDeFooter.addEventListener('click',scrollar);*/
-
-document.addEventListener('DOMContentLoaded', function() {
-    const afetoLink = document.querySelector('.header .logo');
-
-    afetoLink.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        const footer = document.querySelector('.footer');
-
-        footer.scrollIntoView({ behavior: 'smooth' });
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
+
 
 
 
